4124d759dfa5c525ffa9a1455de5181ee4694b54
satanpotato98 <shashwatkumarsk1998@gmail.com>
2023-05-04 16:25:29 +0530

    CICD Check

--- HDFC_CapstoneProject/src/main/java/com/hdfc/employee/controller/EmployeeController.java
+++ HDFC_CapstoneProject/src/main/java/com/hdfc/employee/controller/EmployeeController.java
@@ -2,11 +2,14 @@ package com.hdfc.employee.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hdfc.employee.dto.EmployeeDTO;
@@ -32,7 +35,14 @@ public class EmployeeController {
 	
 	
 	/*
+	 * @RequestMapping(value="/options", method = RequestMethod.OPTIONS)
+	 * ResponseEntity<?> collectionOptions() { return ResponseEntity
+	 * .ok().allow(HttpMethod.GET, HttpMethod.POST, HttpMethod.OPTIONS) .build(); }
+	 * 
+	 * 
+	 * 
 	 * @PostMapping("/add") public Employee addEmployee(@RequestBody EmployeeDTO
 	 * dto) { return empService.addEmployee(dto); }
 	 */
+	 
 }
