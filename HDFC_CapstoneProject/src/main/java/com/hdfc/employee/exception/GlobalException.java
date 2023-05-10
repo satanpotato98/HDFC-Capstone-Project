5f7a2788bef069f9d200e9cf6e674272e3c6e9bb
satanpotato98 <shashwatkumarsk1998@gmail.com>
2023-05-10 16:59:28 +0530

    CICD Check

--- HDFC_CapstoneProject/src/main/java/com/hdfc/employee/exception/GlobalException.java
+++ HDFC_CapstoneProject/src/main/java/com/hdfc/employee/exception/GlobalException.java
@@ -4,13 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 
 @RestControllerAdvice
 public class GlobalException {
 	
 	@ExceptionHandler(InvalidEmployeeIdException.class)
-	public ResponseEntity<String> bookingNotFound() {
+	public ResponseEntity<String> invalidId() {
 		return  new ResponseEntity<String>("Invalid EmployeeID",HttpStatus.EXPECTATION_FAILED);
 	}
+	
 
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<String> typeMismatch() {
+		return  new ResponseEntity<String>("Invalid EmployeeID: enter an positive integer",HttpStatus.EXPECTATION_FAILED);
+	}
+	
 }
